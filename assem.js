fd7c785000b3af9bc230968749d41648dd2aae42
Samuel Pagenkopf <ssaammp@gmail.com>
2018-02-06 21:37:39 -0600

    collect lexed program in array

--- assem.js
+++ assem.js
@@ -19,45 +19,47 @@ const read_file = (err, input) => {
 };
 
 const lex = {
+    multi_line: (code) => {
+        let lexed = lex.line_array(code)
+            .map(line => lex.line(line));
+    },
+
+    line_array: (code) => {
+        const line_iter = /(.*?)(;.*\n|\n)/mg;
+        const lines = [];
+        let line;
+        while ((line = line_iter.exec(code))) {
+            lines.push(line[1]);
+        }
+        return lines;
+    },
+
     line: (line) => {
         // Matching Rules:
         // label:  start of line, or first word ending in :
         // opcode: indented or ".word" not ending in :
         // arg:    the rest of the line :^)
-        const [, label, opcode, arg] = /^(\w+|\s+\w+(?=:)|)[\s:\.]*(\w*)(.*)$/u
+        const [, label, opcode, arg] = /^(\w+|\s+\w+(?=:)|)[\s:\.]*(\w*)(.*)$/
               .exec(line)
               .map(s => s.trim());
 
-        if (label || opcode) {
-            console.log(label, '|', opcode, '|', arg);
-            if (arg && codes[opcode.toUpperCase()]) {
-                const [kind, data] = lex.op_arg(arg);
-                console.log('    > arg:', kind, data);
-            }
-        }
-    },
+        const [kind, data] = arg && codes[opcode.toUpperCase()] ?
+              lex.op_arg(arg) : [null, null];
 
-    multi_line: (code) => {
-        let line_iter = /(.*?)(;.*\n|\n)/umg;
-        let line;
-
-        while ((line = line_iter.exec(code))) {
-            lex.line(line[1]);
-        }
+        return {label, opcode, arg, kind, data};
     },
 
     op_arg: (arg) => {
         const clean_arg = arg.replace(/\s+/, ' ');
 
         const patterns = [
-            ['indrx', /[(](.*), ?x[)]/u],
-            ['indry', /[(](.*)[)] ?, ?y/u],
+            ['indrx', /[(](.*), ?x[)]/i],
+            ['indry', /[(](.*)[)] ?, ?y/i],
             ['ind',   /[(](.*)[)]/],
-            ['addrx', /(.*) ?, ?x/u],
-            ['addry', /(.*) ?, ?y/u],
+            ['addrx', /(.*) ?, ?x/i],
+            ['addry', /(.*) ?, ?y/i],
             ['immed', /#(.*)/],
-            ['add',   /(.*)/],
-            ['none',  /^\s*$/],
+            ['add',   /(.+)/],
         ];
 
         for (const match of patterns) {
@@ -67,7 +69,7 @@ const lex = {
             }
         }
 
-        return ['undefined', arg];
+        return ['none', arg];
     },
 };
 
