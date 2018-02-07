833f2cc9e22d0163157d5ad1bc05d9e5f5c2fca1
Samuel Pagenkopf <ssaammp@gmail.com>
2018-02-07 14:54:18 -0600

    test cases for accumulator and caps

--- test.asm
+++ test.asm
@@ -5,6 +5,9 @@ main ; comment1
 main2:
   main3:
   clc
+  ror a                         ; comment
+  ror A                         ; comment
+  ror aardvark
   lda #100
   lda #100 ; comment2
   sta $0
@@ -19,8 +22,8 @@ main2:
   sta ($100,x)
 	sta (256,x)
 
-  sta ( $100, x )
-  sta ( $100 ) , y
+  sta ( $100, X )
+  sta ( $100 ) , Y
 
   jmp ($10)
   jmp (16)
